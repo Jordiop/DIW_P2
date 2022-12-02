2f8aa57831788ed4f9d1bb4cf9f13bb30a8858b8
Jordi Osarenkhoe Petro <jordiosarenkhoe@outlook.es>
2022-12-02 19:02:36 +0100

    Fixed visual and UI

--- js/projecte.js
+++ js/projecte.js
@@ -9,3 +9,11 @@ function toTop() {
     });
 
 }
+
+function god() {
+    let max = 100;
+    let min = 0;
+    let random = Math.floor(Math.random() * (max - min + 1) + min);
+    random = random + "%";
+    document.getElementById('subprogress').style.width = random;
+}
